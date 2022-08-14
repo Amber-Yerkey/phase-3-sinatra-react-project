8ead9173879d7444419de9c08d13108e31b71612
Amber Frantz <amber.k.frantz@mgail.com>
2022-08-14 10:50:19 -0400

    Cleaned up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,27 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # read
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
-  get "/owners" do
-    owners = Owner.all
-    owners.to_json
-  end
-
-  get "/owners/:id" do
-    owners = Owner.find(params[:id])
-    owners.to_json(include: :pokemons)
-  end
-
-  get "/pokemon" do
+  
+  get "/pokemonWithOwner" do
     pokemon = Pokemon.all
+    pokemon.to_json(include: :owner)
+  end
+
+  # Update
+
+  patch '/pokemon/:id' do
+    pokemon = Pokemon.find(params[:id])
+    pokemon.update(
+      claimed: params[:claimed]
+    )
     pokemon.to_json
   end
 
-  get "/pokemon/:id" do
+  # Delete
+
+  delete '/pokemon/:id' do
     pokemon = Pokemon.find(params[:id])
+    pokemon.destroy
     pokemon.to_json
   end
 
